37956298080ba523d3e894b917a78050f3202db4
Muzaffar Khaydarov <114870653+muzaffarcodes@users.noreply.github.com>
2024-01-04 14:29:32 +0500

    Create names.py

---
+++ names.py
@@ -0,0 +1,12 @@
+# Name sort
+
+ism1 = input("1 - ismni kiriting: ")
+ism2 = input("2 - ismni kiriting: ")
+ism3 = input("3 - ismni kiriting: ")
+ism4 = input("4 - ismni kiriting: ")
+ism5 = input("5 - ismni kiriting: ")
+
+list1 = [ism1,ism2,ism3,ism4,ism5]
+list1.sort()
+print("Ismlarni sortlangani: ",list1)
+
